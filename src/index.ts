f56b86581845de4ca88a91e17258f735457cc0ce
William Tange <will@brt.sexy>
2022-11-15 10:09:43 +0100

    Use DATABASE_URL to init pg

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ build({
       },
     },
   },
+  pg: {
+    connectionString: process.env['DATABASE_URL'],
+  },
 }).then(server => {
   server.listen({ port: 8080 }, (err: Error | null, address: string) => {
     if (err) {
