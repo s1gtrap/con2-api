2213448af2198518037da0b836ab3c9fbb12cb64
William Tange <will@brt.sexy>
2022-11-16 10:13:26 +0100

    Debug Heroku runtime

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,10 @@ const server_1 = require("./server");
         connectionString: process.env['DATABASE_URL'],
     },
 }).then(server => {
-    server.listen({ port: Number(process.env['PORT']) || 8080 }, (err, address) => {
+    server.listen({
+        host: '0.0.0.0',
+        port: Number(process.env['PORT']) || 8080,
+    }, (err, address) => {
         if (err) {
             console.error(err);
             process.exit(1);
--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,13 @@ build({
   },
   pg: {
     connectionString: process.env['DATABASE_URL'],
+    ssl: { rejectUnauthorized: false },
   },
 }).then(server => {
-  server.listen({ port: Number(process.env['PORT']) || 8080 }, (err: Error | null, address: string) => {
+  server.listen({
+    host: '0.0.0.0',
+    port: Number(process.env['PORT']) || 8080,
+  }, (err: Error | null, address: string) => {
     if (err) {
       console.error(err);
       process.exit(1);
